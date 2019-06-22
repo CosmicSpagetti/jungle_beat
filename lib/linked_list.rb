0c64fcf034f144eced628262610d1c31da1ee5b7
billy urrutia <billyurrutia@gmail.com>
2019-06-22 16:51:22 -0600

    updated methods to work for multiple strings

--- lib/linked_list.rb
+++ lib/linked_list.rb
@@ -7,13 +7,31 @@ class LinkedList
     @count = 0
   end
 
-  def append(new_node)
-    @head = Node.new(new_node)
+  def append(new_node_data)
+    node = Node.new(new_node_data)
     @count += 1
+    if @head.nil?
+      @head = node 
+    else 
+     @head.next_node = node
+    end 
   end 
 
   def to_string
-    @head.data
+    current_node = @head
+    array_of_words = []
+    while current_node.next_node != nil 
+      array_of_words << current_node.data
+      current_node = current_node.next_node
+    end 
+    array_of_words << current_node.data
+    array_of_words.join(" ")
   end
 
+  def prepend(new_node_data)
+    node = Node.new(new_node_data)
+    @count += 1 
+    node.next_node = @head 
+    @head = node
+  end 
 end 
