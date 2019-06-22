84f3973402f54796982591c389833578aeaf9f84
billy urrutia <billyurrutia@gmail.com>
2019-06-22 16:51:52 -0600

    added test for multiple elements in list

--- test/linked_list_test.rb
+++ test/linked_list_test.rb
@@ -2,6 +2,7 @@ require './lib/node'
 require 'minitest/autorun'
 require 'minitest/pride'
 require './lib/linked_list'
+require 'pry'
 
 class LinkedListTest < MiniTest::Test 
 
@@ -38,5 +39,24 @@ class LinkedListTest < MiniTest::Test
     list.append("doop")
 
     assert_equal "doop", list.to_string
+  end
+
+  def test_can_append_multiple_new_nodes 
+    list = LinkedList.new
+    list.append("doop")
+    list.append("deep")
+
+    assert_equal "deep", list.head.next_node.data
+    assert_equal 2, list.count
+    assert_equal "doop deep", list.to_string
+  end 
+
+  def test_can_add_to_begining_of_list_with_prepend
+    list = LinkedList.new 
+    list.append("deep")
+    list.prepend("doop")
+
+    assert_equal 2, list.count
+    assert_equal "doop deep", list.to_string
   end
 end 
