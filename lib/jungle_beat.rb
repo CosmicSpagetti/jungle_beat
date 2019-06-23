7771ffbc41d6f18fe04862c0b8b4540979edc2d0
billy urrutia <billyurrutia@gmail.com>
2019-06-23 13:02:18 -0600

    finished iteration 5

--- lib/jungle_beat.rb
+++ lib/jungle_beat.rb
@@ -1,6 +1,17 @@
 class JungleBeat 
   attr_reader :list
+
   def initialize
     @list = LinkedList.new
+  end
+
+  def append(several_words)
+    several_words.split(" ").each do |word|
+      @list.append(word)
+    end
+  end
+
+  def count
+    @list.count
   end
 end
